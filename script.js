86e1a3c8cf55b97c54253a43182d0015bc875b11
Edozie Onyeanusi <phynnedo@gmail.com>
2020-12-21 13:12:38 +0100

    Made changes to comments in the script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const themeSwitch = document.querySelector('input#checkbox[type=checkbox]');
 themeSwitch.addEventListener("change", function(){
-    /* When the toggle switch is checked, the event listener is activated and it causs the theme to alternate between light mode and dark mode */
+    /* When the toggle switch is checked, the event listener is activated and it causes the theme to alternate between the light theme and dark theme */
     if(this.checked){
         document.querySelector('body').classList.add('light')
         document.querySelector('.word').textContent = "Light Mode"
